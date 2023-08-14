772230523e9e6af42f01b5c92fd88a124e46fa32
Snailedlt <43886029+Snailedlt@users.noreply.github.com>
2023-08-14 16:24:14 +0200

    Remove unessessary webpack config

--- payload/payload.config.ts
+++ payload/payload.config.ts
@@ -18,23 +18,12 @@ const groupCollections = (group: string | Record<string, string>, collections: C
     }
   })
 }
-const mockModulePath = path.resolve(__dirname, './emptyModuleMock.js')
 
 export default buildConfig({
   admin: {
     components: {
       beforeDashboard: [BeforeDashboard],
     },
-    webpack: config => ({
-      ...config,
-      resolve: {
-        ...config.resolve,
-        alias: {
-          ...config.resolve?.alias,
-          [path.resolve(__dirname, 'collections/PriceZones/hooks/beforeChange')]: mockModulePath,
-        },
-      },
-    }),
   },
   collections: [
     ...groupCollections({en: 'Delivery', no: 'Leveranse'}, [KmRanges, PriceZones, ProductionCenters]),
